d6a75a9bf422c37e74ff8ef7249aca85c0d7d73a
MX <notanactualemail@gmail.com>
2025-07-02 21:17:40 +0200

    basic for test

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+require("dotenv").config();
+const express = require("express");
+
+const port = 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(process.env.OPENROUTER_API_KEY);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
