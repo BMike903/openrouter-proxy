cccb3157fafcd885f09a7c8f183bf47ae81cb0af
MX <notanactualemail@gmail.com>
2025-07-07 17:59:18 +0200

    basic resending messages

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const qs = require("qs");
 
 const port = 3000;
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Nothing here, but server is working");
@@ -13,8 +14,7 @@ app.get("/returnJson", (req, res) => {
   res.json({ ...req.query });
 });
 
-app.get("/message", async (req, res) => {
-  const message = req.query.message;
+app.post("/message", async (req, res) => {
   try {
     const response = await fetch(
       "https://openrouter.ai/api/v1/chat/completions",
@@ -24,16 +24,13 @@ app.get("/message", async (req, res) => {
           "Authorization": `Bearer ${process.env.OPENROUTER_API_KEY}`,
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          model: "google/gemma-3n-e4b-it:free",
-          messages: [{ "role": "user", "content": String(message) }],
-        }),
+        body: JSON.stringify(req.body),
       }
     );
     const data = await response.json();
     res.json(data);
   } catch (error) {
-    console.log(err);
+    res.send(err);
   }
 });
 
