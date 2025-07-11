d1f4b29bb51d5996ff8646848f5310026857a136
MX <notanactualemail@gmail.com>
2025-07-12 01:42:22 +0200

    cors setup

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const port = 3000;
+const allowedOrigins = [
+  "https://bmike903.github.io",
+  "https://bmike903.github.io/LLM-front",
+];
+
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Blocked by CORS"));
+      }
+    },
+  })
+);
+
+app.options("*", cors());
 
 app.get("/", (req, res) => {
   res.send("Nothing here, but server is working");
